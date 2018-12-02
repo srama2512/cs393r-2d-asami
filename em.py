2215ecc97c5c9ef0fb2511a184c14b1593bcf8cc
srama2512 <sramakrishnan@utexas.edu>
2018-12-02 11:29:51 -0600

    added pruning --- some changes in core filterpy library

--- em.py
+++ em.py
@@ -10,6 +10,7 @@ from process_gt import compute_action_model, process_gt_data
 
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 import pdb
 import math
 import argparse
@@ -46,6 +47,8 @@ def residual_fn(z, z2):
 class EKFforward(ExtendedKalmanFilter):
     def __init__(self, *args, action_cov=None, **kwargs):
         super().__init__(*args, **kwargs)
+        self.innovation_count = 0.0
+        self.large_innovation_flag = False
         if action_cov is None:
             self.action_cov = np.eye(self.dim_u)
         else:
@@ -220,7 +223,7 @@ class EM(object):
                              [           j21,            j22, -1]])
         return HJacobian_at
 
-    def Estep(self, data):
+    def Estep(self, data, prune_flag=False):
         # data - list of observationTuples
         # Note: height, bearing are after taking command for dt
         self._initialize_em()
@@ -228,12 +231,17 @@ class EM(object):
         self.forward_observations = []
         self.backward_distance_estimates = []
         self.backward_observations = []
+        prev_obs_seen = 0.0
+        tot_obs = 0.0
+
+        self.forward_model.innovation_count = 0.0
         # ==== forward model prediction ====
         for i, data_t in enumerate(data):
-            ht, bear, bid, cmd, dt, _ = data_t
+            ht, bear, bid, cmd, dt, dist = data_t
             act = self.action_mean_model.get(cmd) * dt
             self.forward_model.predict(u=act)
             # print('gt : {}'.format(gt_data[i+1]))
+            prev_obs_seen += dt
 
             HJacobian_at = None
             hx = None
@@ -251,8 +259,12 @@ class EM(object):
                 mu_obs_t = H.dot(mu_alpha) + hx(mu_alpha) - H.dot(mu_alpha)
                 sigma_obs_t = H.dot(sigma_alpha).dot(H.T) + self.sensor_varn_model
                 self.log_likelihood += multivariate_normal.logpdf(obs, mean=mu_obs_t, cov=sigma_obs_t)
-            self.forward_model.update(obs, HJacobian_at, hx, residual=residual_fn)
+                tot_obs += 1
 
+            self.forward_model.update(obs, HJacobian_at, hx, residual=residual_fn, Dt=prev_obs_seen)
+            # Prune observations that cause too large an innovation
+            if self.forward_model.large_innovation_flag and prune_flag:
+                data[i] = observationTuple(None, None, None, cmd, dt, dist)
 
             forward_params = self.forward_model.get_params()
             self.alphas.append(forward_params)
@@ -260,6 +272,7 @@ class EM(object):
                 d_to_beacon = Lnorm(forward_params[0][:2] - self.bpos[bid][:2])
                 self.forward_distance_estimates.append(d_to_beacon)
                 self.forward_observations.append(d_to_beacon)
+                prev_obs_seen = 0.0
 
         # ==== backward model prediction ====
         for i, data_t in enumerate(list(reversed(data))):
@@ -291,9 +304,11 @@ class EM(object):
 
         # ==== gamma model prediction ====
         for a, b in zip(self.alphas, self.betas):
-            gamma = mul_gaussians(a, b)
+            gamma = deepcopy(a)#mul_gaussians(a, b)
             gamma[0][2] = _norm_angle(gamma[0][2])
             self.gammas.append(gamma)
+
+        print('Number of invalid observations: {}/{}'.format(self.forward_model.innovation_count, tot_obs))
 
     def Mstep(self, data):
         # data - list of observationTuples
@@ -336,8 +351,8 @@ class EM(object):
         mu_cmds = mu_cmds / (n_cmds.reshape((-1,1)) + 1e-8)
         mu_cmds[n_cmds == 0] = 0
 
-        with np.printoptions(formatter={'float': '{: 10.3f}'.format}):
-            print('mu_cmds update : \n{}'.format(np.column_stack([self.action_mean_model.cmd_mus, mu_cmds])))
+        #with np.printoptions(formatter={'float': '{: 10.3f}'.format}):
+        #    print('mu_cmds update : \n{}'.format(np.column_stack([self.action_mean_model.cmd_mus, mu_cmds])))
         mu_cmds += self.action_mean_model.cmd_mus
 
         # mu_cmds = np.concatenate([mu_cmds[:, :2], np.arctan2(mu_cmds[:, 3], mu_cmds[:, 2])[:, np.newaxis]], axis=1)
@@ -366,15 +381,18 @@ class EM(object):
         self.sensor_varn_model = np.diag([sigma_1_sqr, sigma_2_sqr])
         self.prior_mean_model  = np.copy(self.gammas[0][0])
         self.prior_varn_model  = np.copy(self.gammas[0][1])
-        print('sigma_1 : {:.3f}, sigma_2 : {:.3f}'.format(self.sensor_varn_model[0, 0], self.sensor_varn_model[1, 1]))
+        print('sigma_1 : {:.3f}, sigma_2 : {:.3f}'.format(math.sqrt(self.sensor_varn_model[0, 0]), math.sqrt(self.sensor_varn_model[1, 1])))
         # pdb.set_trace()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data', type=str, default='2d_asami_data.txt')
+    parser.add_argument('--save_path', type=str, default='plots')
     parser.add_argument('--n_iter', type=int, default=10)
     parser.add_argument('--seed', type=int, default=123)
     parser.add_argument('--n_plot', type=int, default=10)
+    parser.add_argument('--display', type=str, default='yes')
+
     args = parser.parse_args()
 
     np.random.seed(args.seed)
@@ -386,9 +404,18 @@ if __name__ == '__main__':
     gt_data_pairs = np.array(list(gt_data_pairs))
     em.sensor_mean_model.fit(gt_data_pairs[:, 1], gt_data_pairs[:, 0])
 
+    try:
+        os.mkdir(args.save_path)
+    except:
+        pass
+
     for it in range(0, args.n_iter):
         print('=====> Iteration {:d}'.format(it))
-        em.Estep(data)
+        if it == 0:
+            prune_flag = True
+        else:
+            prune_flag = False
+        em.Estep(data, prune_flag=prune_flag)
         print('=====> Log Likelihood {:.3f}'.format(em.log_likelihood))
         #plt.subplot(2, 1, 1)
         #plt.plot(em.forward_distance_estimates, em.forward_observations)
@@ -420,7 +447,7 @@ if __name__ == '__main__':
             # ax.plot(x, y, t, label='gt curve', linestyle='dashed')
             # plt.legend()
 
-            fig = plt.figure(1)
+            fig1 = plt.figure(1)
             # plt.subplot(2, 1, 1)
             x = [alpha[0][0] for alpha in em.alphas]
             t = list(range(len(em.alphas)))
@@ -435,8 +462,9 @@ if __name__ == '__main__':
             plt.title('Betas x value')
             # plt.plot(range(gt_data.shape[0]), gt_data[:, 0], linestyle='dashed')
             plt.legend()
+            plt.savefig(os.path.join(args.save_path, f'x_plot_{it:04}.png'))
 
-            fig = plt.figure(2)
+            fig2 = plt.figure(2)
             # plt.subplot(2, 1, 1)
             y = [alpha[0][1] for alpha in em.alphas]
             t = list(range(len(em.alphas)))
@@ -451,8 +479,9 @@ if __name__ == '__main__':
             plt.title('Betas y value')
             # plt.plot(range(gt_data.shape[0]), gt_data[:, 1], linestyle='dashed')
             plt.legend()
+            plt.savefig(os.path.join(args.save_path, f'y_plot_{it:04}.png'))
 
-            fig = plt.figure(3)
+            fig3 = plt.figure(3)
             # plt.subplot(1, 1, 1)
             theta = np.unwrap(np.array([alpha[0][2] for alpha in em.alphas]))
             t = list(range(len(em.alphas)))
@@ -467,8 +496,9 @@ if __name__ == '__main__':
             plt.title('Betas theta value')
             # plt.plot(range(gt_data.shape[0]), gt_data[:, 2], linestyle='dashed')
             plt.legend()
+            plt.savefig(os.path.join(args.save_path, f'theta_plot_{it:04}.png'))
 
-            fig = plt.figure(4)
+            fig4 = plt.figure(4)
             distances = np.linspace(1000, 4000, 1000)
             plt.plot(distances, em.sensor_mean_model.predict(distances))
             gt_data_pairs = filter(lambda x: x[0] is not None, [[d.height, d.dist] for d in data])
@@ -480,6 +510,14 @@ if __name__ == '__main__':
             plt.xlabel('Distances')
             plt.ylabel('Observations')
             plt.title('Learned sensor model plot')
-            plt.show()
+            plt.savefig(os.path.join(args.save_path, f'sensor_plot_{it:04}.png'))
+
+            if args.display == 'yes':
+                plt.show()
+
+            fig1.clf()
+            fig2.clf()
+            fig3.clf()
+            fig4.clf()
 
         em.Mstep(data)
